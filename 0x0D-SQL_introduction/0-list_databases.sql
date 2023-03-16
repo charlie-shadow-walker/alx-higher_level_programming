c3838e26b4a7bdeb243df73a12d4aa19e50cd9e9
charlie-shadow-walker <mastercharlie1998@gmail.com>
2023-03-16 02:34:08 -0700

    sql statements

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,2 @@
+-- script to show all the databases in the mysql server
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- script to create a database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
