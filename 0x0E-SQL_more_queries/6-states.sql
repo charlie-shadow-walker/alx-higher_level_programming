4eef17c05737e5e874aba295c98b335a8e49145e
charlie-shadow-walker <mastercharlie1998@gmail.com>
2023-03-16 05:24:22 -0700

    sql queries

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- script to create a database and the state
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);
+
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- selecting all the cities in califonia
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	        FROM `states`
+	       WHERE `name` = "California")
+ORDER BY `id`;
